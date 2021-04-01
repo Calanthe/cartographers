19cea645342949d20bc7a27925be05d53d75f96b
Zosia <korczzofia@gmail.com>
2021-04-01 12:57:20 +0200

    map border fixes

--- src/modules/GenerateMapBtn.js
+++ src/modules/GenerateMapBtn.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function GenerateMapBtn(props) {
 	return (
-		<div className="cartographers__button">
-			<button onClick={props.onGenerateMap}>Generate new map</button>
+		<div className="cartographers__button-wrapper">
+			<button className="cartographers__button" onClick={props.onGenerateMap}>Generate a new map</button>
 		</div>
 	);
 };
